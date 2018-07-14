19a295508c4f47e13404536ea9a8f9990493d117
denniz-10 <denilson_medrano@hotmail.com>
2018-07-13 21:31:14 -0300

    dataTable - gerando excel e pdf atraves do formulario

--- Senai.Chamados.Web/App_Start/RouteConfig.cs
+++ Senai.Chamados.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Senai.Chamados.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Conta", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
